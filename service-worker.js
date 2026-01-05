c21143f59a0c33b6abe080ad10ed8715b9990066
alsisanty-lgtm <alsisanty@gmail.com>
2026-01-05 16:22:59 +0100

    Update CACHE_NAME to guardias-v34

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "guardias-v33";
+const CACHE_NAME = "guardias-v34";
 const FILES = [
   "/",
   "/index.html",
@@ -23,6 +23,7 @@ self.addEventListener("fetch", event => {
     caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
 
 
 
