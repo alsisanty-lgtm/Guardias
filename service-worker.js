cdde0e770b97d55364431d875fa100338557b522
alsisanty-lgtm <alsisanty@gmail.com>
2026-01-05 14:37:52 +0100

    Update service worker cache configuration

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
-const CACHE_NAME = "guardias-pwa-v5";
-
-const FILES_TO_CACHE = [
+const CACHE_NAME = "guardias-v31";
+const FILES = [
   "/",
   "/index.html",
   "/manifest.json",
@@ -9,24 +8,22 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
-    )
-  );
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
 
 
 
