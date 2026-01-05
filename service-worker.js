f9d8ea9d7acbbec8ce1fc56220d7ffc10fc9f6b5
alsisanty-lgtm <alsisanty@gmail.com>
2026-01-05 14:00:06 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,33 @@
-const CACHE_NAME="guardias-pro-cache-v4";
-const urlsToCache=["/index.html","/manifest.json","icon-192.png","icon-512.png"];
+const CACHE_NAME = "guardias-pwa-v1";
 
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(resp=>resp||fetch(e.request)));
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
 });
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
+
 
 
 
