4cd03d4e30871089011c23b20cac49c809725b35
alsisanty-lgtm <alsisanty@gmail.com>
2026-01-07 09:39:23 +0100

    Update CACHE_NAME to guardias-alsi-v11

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "guardias-alsi-v10";
+const CACHE_NAME = "guardias-alsi-v11";
 
 const FILES = [
   "/Guardias/",
@@ -31,6 +31,7 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
 
