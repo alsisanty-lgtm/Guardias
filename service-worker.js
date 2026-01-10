f4e10006e08cf03d72d9da0114de91c9d0c57404
alsisanty-lgtm <alsisanty@gmail.com>
2026-01-10 07:55:43 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "guardias-alsi-v45";
+const CACHE_NAME = "guardias-alsi-v46";
 
 const FILES = [
   "/Guardias/",
@@ -31,6 +31,7 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
 
